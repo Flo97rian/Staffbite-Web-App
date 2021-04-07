b63a4fb6478e7a9b097cbc3cb9e88bdb4edbc1f0
Florian <f.zellmann@wtnet.de>
2021-04-07 15:46:05 +0200

    Frontend eingefügt, AWS Standard Auth

---
+++ src/views/Index.js
@@ -0,0 +1,51 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React, { useState } from "react";
+// node.js library that concatenates classes (strings)
+import classnames from "classnames";
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  NavItem,
+  NavLink,
+  Nav,
+  Progress,
+  Table,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
+
+import Header from "../components/Headers/Header.js";
+
+const Index = (props) => {
+  return (
+    <>
+      <Header />
+      {/* Page content */}
+      <Container className="mt--7" fluid>
+      </Container>
+    </>
+  );
+};
+
+export default Index;
