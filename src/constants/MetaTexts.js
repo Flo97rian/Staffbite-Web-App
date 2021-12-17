4fcfb9df08fa358a98ffb675647d799e5be3151f
Danny <Daniel.Zellmann@wi.hs-flensburg.de>
2021-12-17 17:31:04 +0100

    MetaTexte Versuch (Fokus UI - SEO noch nicht)

--- src/constants/MetaTexts.js
+++ src/constants/MetaTexts.js
@@ -1,20 +1,19 @@
 // Title Länge maixmal 100 Zeichen
 // Description Länge maximal 255 Zeichen
 // gerne Keywords benutzen
-export const LANDING_TITLE = "Staffbite - Dein digitaler Schichtplan online & per App";
-export const LANDING_DESCRIPTION = "Wir digitalisieren Schichtpläne für die Gastronomie, Cafes, Einzelhändler und viele mehr. Durch einen online Schichtplaner und einer automatisierten Befüllung der Schichtpläne ermöglichen wir die Schichtplanung in wenigen Mitnuten.";
-export const SCHICHTPLAN_TITLE = "Staffbite - Dein digitaler Schichtplan online & per App";
-export const SCHICHTPLAN_DESCRIPTION = "Staffbite - Dein digitaler Schichtplan online & per App";
-export const THEMEN_TITLE = "Staffbite - Dein digitaler Schichtplan online & per App";
-export const THEMEN_DESCRIPTION = "Staffbite - Dein digitaler Schichtplan online & per App";
-export const THEMEN_CLOUDSERVICE_TITLE = "Staffbite - Dein digitaler Schichtplan online & per App";
-export const THEMEN_CLOUDSERVICE_DESCRIPTION = "Staffbite - Dein digitaler Schichtplan online & per App";
-export const IMPRESSUM_TITLE = "Staffbite - Dein digitaler Schichtplan online & per App";
-export const IMPRESSUM_DESCRIPTION = "Staffbite - Dein digitaler Schichtplan online & per App";
-export const FAQ_TITLE = "Staffbite - Dein digitaler Schichtplan online & per App";
-export const FAQ_DESCRIPTION = "Staffbite - Dein digitaler Schichtplan online & per App";
-export const KONTAKT_TITLE = "Staffbite - Dein digitaler Schichtplan online & per App";
-export const KONTAKT_DESCRIPTION = "Staffbite - Dein digitaler Schichtplan online & per App";
-export const ABOUTUS_TITLE = "Staffbite - Dein digitaler Schichtplan online & per App";
-export const ABOUTUS_DESCIPTION = "Staffbite - Dein digitaler Schichtplan online & per App";
-
+export const LANDING_TITLE = "Staffbite - Dein digitaler Schichtplan online & als App";
+export const LANDING_DESCRIPTION = "Wir bieten dir digitale Schichtpläne für die Gastronomie, Hotellerie, den Einzelhandel und viele mehr. Durch einen online Schichtplan und unsere Automatisierung der Befüllung kannst du jede Woche mehrere Stunden Zeit sparen.";
+export const SCHICHTPLAN_TITLE = "Staffbite - Unsere Funktionalitäten";
+export const SCHICHTPLAN_DESCRIPTION = "Hier findest du einen Überblick über unsere Funktionalitäten. Von deinen ersten Schritten zum digitalen Schichtplan, der automatisierten Befüllung bis zur Veröffentlichung des fertigen Plans. Natürlich gibt es auch jede Menge Vorteile für dein Team!";
+export const THEMEN_TITLE = "Staffbite - Themen";
+export const THEMEN_DESCRIPTION = "Auf dieser Seite beantworten wir Fragen, geben Tipps & Tricks an die Hand und halten dich auf dem laufenden!";
+export const THEMEN_CLOUDSERVICE_TITLE = "Staffbite - Dein Online Schichtplan durch die Cloud";
+export const THEMEN_CLOUDSERVICE_DESCRIPTION = "In diesem Beitrag gehen wir auf die Frage ein warum Staffbite ein Cloud Service ist und welche Vorteile dies für unsere Kunden hat.";
+export const IMPRESSUM_TITLE = "Staffbite - Impressum";
+export const IMPRESSUM_DESCRIPTION = "Hier findest du Kontaktmöglichkeiten und vieles mehr.";
+export const FAQ_TITLE = "Staffbite - FAQ";
+export const FAQ_DESCRIPTION = "Die häufigsten Fragen werden hier direkt beantwortet. Vom kostenlosen Probemonat über Schichtplan Vorlagen und dem Schichten tauschen wird alles beantwortet.";
+export const KONTAKT_TITLE = "Staffbite - Kontakt";
+export const KONTAKT_DESCRIPTION = "Bei Fragen kannst du dich jederzeit an uns wenden. Lass uns einfach deine Kontaktdaten da und wir melden uns schnellstmöglich bei dir!";
+export const ABOUTUS_TITLE = "Staffbite - Über uns";
+export const ABOUTUS_DESCIPTION = "Du willst mehr über die beiden Gründer von Staffbite erfahren? Dann bist du hier genau richtig.";
